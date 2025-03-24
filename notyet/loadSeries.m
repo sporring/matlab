7336abc2974c66774878692bb9fd6d63183cd175
Jon Sporring <sporring@di.ku.dk>
2025-03-24 11:48:38 +0000

    added progress bar

--- notyet/loadSeries.m
+++ notyet/loadSeries.m
@@ -1,4 +1,4 @@
-function I = loadSeries(srcPath,filePattern)
+function I = loadSeries(srcPath,filePattern,VERBOSE)
 % LOADSERIES load a sequence of images
 %
 % Syntax
@@ -8,16 +8,26 @@ function I = loadSeries(srcPath,filePattern)
 %   I - a 3D images
 %   srcPath - the path to a directory containing a sequence of same-size and format 2D images
 %   filePattern - the file pattern in src to read in lexicographical order
+%   VERBOSE - optional verbose flag. If true, then will show a waitbar window
 %
 % Read a series of images in lexicographical order and return as a 3D
 % image.
 %
 % 2025/03/18, Jon Sporring
 
+if nargin < 3
+    VERBOSE = false;
+end
 lst = dir(fullfile(srcPath,filePattern));
 [~,ind]=sort({lst.name});
 I = imread(fullfile(lst(1).folder,lst(1).name));
 I = repmat(I,[1,1,length(lst)]);
+if VERBOSE
+    waitbar(0,"loadSeries: Reading images")
+end
 for i = 1:length(lst)
     I(:,:,i) = imread(fullfile(lst(ind(i)).folder,lst(ind(i)).name));
+    if VERBOSE
+       waitbar(i/length(lst),"loadSeries: Reading images")
+    end
 end
